fc5f48cd5d749284dda6a7879da81933f36ce6d9
Derek Knight (mac) <Derek@GordonKnight.co.uk>
2016-07-30 15:25:06 +1200

    Initial Commit

---
+++ Defender/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Defender
+//
+//  Created by Derek Knight on 30/07/16.
+//  Copyright © 2016 Derek Knight. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Defender/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Defender
+//
+//  Created by Derek Knight on 30/07/16.
+//  Copyright © 2016 Derek Knight. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
