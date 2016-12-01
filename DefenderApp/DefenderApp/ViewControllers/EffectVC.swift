663a58287ccf3d13dd7b6b0ee1efd5b106edd95a
Derek Knight <derek.knight@orionhealth.com>
2016-12-02 11:37:43 +1300

    Added name and type label to Effect page

--- DefenderApp/DefenderApp/ViewControllers/EffectVC.swift
+++ DefenderApp/DefenderApp/ViewControllers/EffectVC.swift
@@ -14,6 +14,8 @@ class EffectVC: UIViewController {
     @IBOutlet weak var shade: ShadeControl!
     @IBOutlet weak var slotLabel: UILabel!
     @IBOutlet weak var chassis: EffectControl!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var powerLED: LEDControl!
 
     let bgColours: [Int : UIColor] = [1 : UIColor(red: 0.30, green: 0.30, blue: 0.41, alpha: 1.0),
@@ -84,6 +86,8 @@ class EffectVC: UIViewController {
         fullBackgroundColour = bgColours[effect?.colour ?? 0] ?? UIColor.slotBackground
         if appeared {
             if slotNumber != nil { slotLabel.text = "\(self.slotNumber! + 1)" } else { slotLabel.text = "" }
+            typeLabel.text = effect?.type.rawValue.uppercased() ?? ""
+            nameLabel.text = effect?.name?.uppercased() ?? ""
             if effect == nil {
                 state = .disabled
             } else {
